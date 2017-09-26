d217e81cd64912bc1d89b11b2ab5ad9294bd01d3
thiagoprocaci <you@example.com>
2017-09-26 16:37:56 -0300

    dataset

---
+++ dataset/biology.sql
@@ -0,0 +1,7 @@
+select count(*) from biology_user_profile
+union all
+select count(*) from biology_question
+union all
+select count(*) from biology_answer
+union all
+select count(*) from biology_comment
---
+++ dataset/chemistry.sql
@@ -0,0 +1,7 @@
+select count(*) from chemistry_user_profile
+union all
+select count(*) from chemistry_question
+union all
+select count(*) from chemistry_answer
+union all
+select count(*) from chemistry_comment
